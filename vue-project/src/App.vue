57a5a8152c564f3562bc04b4022ccde26f507266
Support <support@paymebuilder.com>
2023-01-04 17:18:36 +0100

    51-api-docs: add example target app.

---
+++ vue-project/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue'
+
+const msg = ref('Credit Card checkout Example')
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <a href="/card.html?signature=lCC4%2FhOExMqDqrv0RmxEhMqvZjiH7mGIjlqz%2Bcd13OKiY%2FcoYZHGTV4zos8jDbLQR0gyGe%2BX7%2BdXvg3SRsvDQUSVC7LPJdSA08v64Cjj4FlMRtlhJ6GP7SMkPlxnuvAVONQaNsTZxET1JfTQ6A52LSdKSsYIWQFzaG8qPwYGPSBKH9z7zv4moOHd9g%2F4DDfC&amount=1000&currency=EUR">
+Payment</a>
+</template>
